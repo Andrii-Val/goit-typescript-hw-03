516170d08751557066385a17e4fa698767f398a8
Andrii-Val <andrii.val1006@gmail.com>
2023-10-22 08:17:23 +0300

    1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,50 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number = Math.random();
+  
+    getSignature() {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+    constructor(key: Key) {
+      this.key = key;
+    }
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+    comeIn(person: Person) {
+      this.tenants.push(person);
+    }
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (this.key.getSignature() === key.getSignature()) {
+        this.door = true;
+      }
+    }
+  }
+  
+  const key = new Key();
+  
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  
+  house.comeIn(person);
+  
+  export {};
